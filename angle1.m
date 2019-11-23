876ee206c1717cb182c7c37747757a3e0617b0bd
velocityjet1513 <justinxu108@gmail.com>
2019-11-22 20:04:49 -0500

    Update angle1.m

--- angle1.m
+++ angle1.m
@@ -14,10 +14,7 @@ for k = 1:numberoffiles
     %carboncarbon
 %4-2-10
 
-[vector1,vector2] = calcvector(4,2,10,x,y,z);
-anglea = dotproduct(vector1,vector2);
-%2-10-8
-[vector1,vector2] = calcvector(2,10,8,x,y,z);
+[vector1,vector2] = calcvector(2,10,8,xz);
 angleb = dotproduct(vector1,vector2);
 
 angleCCC(k) = (anglea+angleb)/2;
@@ -45,9 +42,6 @@ angleCCH(k) = (anglea+angleb+anglec+angled)/4;
 %2-4-1
 [vector1,vector2] = calcvector(2,4,1,x,y,z);
 anglea = dotproduct(vector1,vector2);
-%2-4-5
-[vector1,vector2] = calcvector(2,4,5,x,y,z);
-angleb = dotproduct(vector1,vector2);
 %10-8-12
 [vector1,vector2] = calcvector(10,8,12,x,y,z);
 anglec = dotproduct(vector1,vector2);
@@ -70,14 +64,12 @@ vector2 = [x(c,1)-x(b,1) y(c,1)-y(b,1) z(c,1)-z(b,1)];
 end
 
 % calculate angles
-function angle = dotproduct(vector1,vector2)
 
 CosTheta = dot(vector1,vector2)/(norm(vector1)*norm(vector2));
 angle = acosd(CosTheta);
 
 end
 
-function plotting(angleCCC,angleCCH,angleCCF);
 
 xvalue = [0.005:0.005:0.4]
 
@@ -105,19 +97,6 @@ figure(1)
 plot(xvalue,angleCCH,'-r','LineWidth',1.5);    
 
   
-ax = gca; grid on;
-ax.Title.FontSize = 20;
-ax.XAxis.FontSize = 20;
-ax.YAxis.FontSize = 20;
-ax.YAxis.Color = 'k';
-xlim([0,0.5]);
-%ylim([0 1.1]);
-xlabel('Axial Strain','Interpreter','latex','FontSize',20,'FontWeight','bold','Color','k');
-ylabel('Angle Between Bonds (Degrees)','Interpreter','latex','FontSize',20,'FontWeight','bold','Color','k');
-print('-depsc','-r1200',sprintf('1_CCH'));
-close(figure(1));
-
-figure(1)
 
 
 plot(xvalue,angleCCF,'-r','LineWidth',1.5);    
